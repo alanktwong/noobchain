a6359d45b874b2feb20383bec696cbb810c31660
Alan Wong <awong@tacitknowledge.com>
2018-10-27 22:03:15 -0400

    Add transaction domain service

---
+++ src/main/java/com/pb/noobchain/service/TransactionDomainService.java
@@ -0,0 +1,12 @@
+package com.pb.noobchain.service;
+
+import java.security.PublicKey;
+
+import com.pb.noobchain.domain.Transaction;
+import com.pb.noobchain.domain.TransactionInput;
+import com.pb.noobchain.domain.TransactionOutput;
+import com.pb.noobchain.domain.Wallet;
+
+public interface TransactionDomainService
+{
+}
