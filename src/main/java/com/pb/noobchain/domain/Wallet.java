721f6007539c55a958483f23e16f89cdc23a5547
Alan Wong <awong@tacitknowledge.com>
2018-10-27 20:32:17 -0400

    Create wallet

---
+++ src/main/java/com/pb/noobchain/domain/Wallet.java
@@ -0,0 +1,50 @@
+package com.pb.noobchain.domain;
+
+
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+public class Wallet {
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public Wallet(){
+        generateKeyPair();
+    }
+
+    public void generateKeyPair(Wallet wallet) {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            // Initialize the key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random);   //256 bytes provides an acceptable security level
+            KeyPair keyPair = keyGen.generateKeyPair();
+            // Set the public and private keys from the keyPair
+            wallet.setPrivateKey(keyPair.getPrivate());
+            wallet.setPublicKey(keyPair.getPublic());
+        }catch(Exception e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public PrivateKey getPrivateKey()
+    {
+        return privateKey;
+    }
+
+    public void setPrivateKey(final PrivateKey privateKey)
+    {
+        this.privateKey = privateKey;
+    }
+
+    public PublicKey getPublicKey()
+    {
+        return publicKey;
+    }
+
+    public void setPublicKey(final PublicKey publicKey)
+    {
+        this.publicKey = publicKey;
+    }
+}
