09e189784be1aae50501f961004a4c9adb58a357
Alan Wong <awong@tacitknowledge.com>
2018-10-27 21:06:54 -0400

    Initial commit of transaction inputs and outputs

--- src/main/java/com/pb/noobchain/domain/TransactionInput.java
+++ src/main/java/com/pb/noobchain/domain/TransactionInput.java
@@ -2,4 +2,33 @@ package com.pb.noobchain.domain;
 
 public class TransactionInput
 {
+    //Reference to TransactionOutputs -> transactionId
+    private String transactionOutputId;
+
+    //Contains the Unspent transaction output
+    private TransactionOutput unspentTransactionOutput;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public String getTransactionOutputId()
+    {
+        return transactionOutputId;
+    }
+
+    public void setTransactionOutputId(final String transactionOutputId)
+    {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public TransactionOutput getUnspentTransactionOutput()
+    {
+        return unspentTransactionOutput;
+    }
+
+    public void setUnspentTransactionOutput(final TransactionOutput unspentTransactionOutput)
+    {
+        this.unspentTransactionOutput = unspentTransactionOutput;
+    }
 }
--- src/main/java/com/pb/noobchain/domain/TransactionOutput.java
+++ src/main/java/com/pb/noobchain/domain/TransactionOutput.java
@@ -1,5 +1,80 @@
 package com.pb.noobchain.domain;
 
+import java.security.PublicKey;
+
+import com.pb.noobchain.service.HashUtil;
+
 public class TransactionOutput
 {
+    private String id;
+
+    //also known as the new owner of these coins.
+    private PublicKey recipient;
+
+    //the amount of coins they own
+    private float value;
+
+    //the id of the transaction this output was created in
+    private String parentTransactionId;
+
+    //Constructor
+    public TransactionOutput(PublicKey recipient, float value, String parentTransactionId) {
+        this.setRecipient(recipient);
+        this.setValue(value);
+        this.setParentTransactionId(parentTransactionId);
+
+        this.setId(createId(this));
+    }
+
+    private String createId(TransactionOutput txnOutput)
+    {
+        final String input = HashUtil.getStringFromKey(txnOutput.getRecipient()) + Float.toString(txnOutput.getValue()) + txnOutput.getParentTransactionId();
+        return HashUtil.applySha256(input);
+    }
+
+
+    //Check if coin belongs to you
+    public boolean isMine(PublicKey publicKey) {
+        return (publicKey == recipient);
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(final String id)
+    {
+        this.id = id;
+    }
+
+    public PublicKey getRecipient()
+    {
+        return recipient;
+    }
+
+    public void setRecipient(final PublicKey recipient)
+    {
+        this.recipient = recipient;
+    }
+
+    public float getValue()
+    {
+        return value;
+    }
+
+    public void setValue(final float value)
+    {
+        this.value = value;
+    }
+
+    public String getParentTransactionId()
+    {
+        return parentTransactionId;
+    }
+
+    public void setParentTransactionId(final String parentTransactionId)
+    {
+        this.parentTransactionId = parentTransactionId;
+    }
 }
